803f495d7d191395a12dd8c151242daf00bba1e9
melijauregui <mjauregui@fi.uba.ar>
2025-03-18 11:18:38 -0300

    [fix] info config

--- client/main.go
+++ client/main.go
@@ -88,7 +88,7 @@ func InitLogger(logLevel string) error {
 // PrintConfig Print all the configuration parameters of the program.
 // For debugging purposes only
 func PrintConfig(v *viper.Viper) {
-	log.Infof("action: config | result: success | client_id: %s | server_address: %s | loop_amount: %v | loop_period: %v | log_level: %s | nombre %s | apellido %s | dni %s | nacimiento %s | numero %v",
+	log.Infof("action: config | result: success | client_id: %s | server_address: %s | loop_amount: %v | loop_period: %v | log_level: %s | nombre: %s | apellido: %s | dni: %v | nacimiento: %s | numero: %v",
 		v.GetString("id"),
 		v.GetString("server.address"),
 		v.GetInt("loop.amount"),
